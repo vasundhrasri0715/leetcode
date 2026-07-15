53a2c1bfbd9ef1bf406a663f1b6742d283e3c36f
vasundhara sri <vasundhrasri07@gmail.com>
2026-07-15 20:09:00 +0530

    Add fibonaccisequence solution

---
+++ leetcode problem/fibonaccisequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        int a=0;
+        int b=1;
+        int c=0;
+        for(int i=1;i<n;i++){
+             c=a+b;
+             a=b;
+             b=c;
+        }
+        return c;
+        
+    }
+}
