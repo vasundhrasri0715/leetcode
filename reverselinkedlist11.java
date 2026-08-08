7207c4f710eb3e063d129019a91085e3a2d8ad1e
vasundhara sri <vasundhrasri07@gmail.com>
2026-08-08 21:49:27 +0530

    Add reverselinkedlist11 solution

---
+++ reverselinkedlist11.java
@@ -0,0 +1,30 @@
+ class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        
+        if (head == null || left == right) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        // Move prev to the node before left
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+
+        // Reverse the required portion
+        for (int i = 0; i < right - left; i++) {
+            ListNode next = curr.next;
+            curr.next = next.next;
+            next.next = prev.next;
+            prev.next = next;
+        }
+
+        return dummy.next;
+    }
+}
